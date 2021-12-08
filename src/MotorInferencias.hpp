f092336bbfee61cff0e55c757459a7c48e8b1a8d
Elena Pérez González-Tablas <elenatablas1@gmail.com>
2021-12-08 17:44:28 +0100

    src: initial

--- src/MotorInferencias.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __MotorInferencias_H__
-#define __MotorInferencias_H__
-
-// Definiciones y declaraciones públicas
-
-bool motorInferencias(char *meta, char **BH, char **BC)
-
-#endif
--- src/practica2-SSII.cpp
+++ src/practica2-SSII.cpp
@@ -1,25 +1,7 @@
-#include <iostream>
-
-
+#include <iostream> // Variables cin y cout
+#include <set>
+#include <regex>
 using namespace std;
-
-void 
-entradaBH(int numBH)
-{
-    /*for(int istd=0; i<numBH; i++)
-    {
-
-    }*/
-    char * objetivo;
-}
-
-
-void 
-entradaBC(int numBC)
-{
-    // for(int i=0; i<numBC; i++)
-     
-}
 
 enum 
 TipoRep { conjuncion, disyuncion };
@@ -27,16 +9,17 @@ TipoRep { conjuncion, disyuncion };
 struct 
 ReglaRep
 {
-    char **antecedentes;
+    int numRegla;
+    std::set<std::string> antecedentes;
     enum TipoRep tipo;
-    char *consecuentes;
+    std::string consecuentes;
     float facCerBC;
 };
 
 struct 
 HechoRep
 {
-    char **hecho;
+    std::set<std::string> hecho;
     float facCerBH;
 };
 
@@ -44,12 +27,188 @@ typedef enum TipoRep Tipo;
 typedef struct ReglaRep Regla;
 typedef struct HechoRep Hecho;
 
+//regexHecho = ()
+
+std::string
+entradaBH()
+{
+    //std::regex regexHecho ("([^,]+)");
+    //std::regex regexFactor ("(\d+(\.\d+)?)$");
+    std::regex regexLinea("[^,]+)(,\s+FC\s*=\s*)(\d+(\.\d+)?$");
+    Hecho hecho;
+    std::set<Hecho> bh;
+    unsigned numBH;
+    cin >> numBH; // Número de casos
+    
+    cin.get(); //por el retorno de carrol
+    for(int i=0; i<numBH; i++)
+    {
+        std::string linea = "conduce>3, FC=1";
+        //getline(cin, linea);
+        std::smatch match;
+        if (std::regex_search(linea, match, regexLinea)) {
+            std::cout << match.str(1) << std::endl;
+            //hecho.hecho.insert(match.str(1));
+            //hecho.facCerBH = 2;
+           // bh.insert(hecho);
+        }
+    }
+    std::string objetivo = "Hola";
+    //getline(cin, objetivo);
+    cin >> objetivo;
+    return objetivo;
+}
+/*
+
 void 
+entradaBC()
+{
+    Regla regla;
+    std::set<Regla> bc;
+    unsigned numBC;
+    unsigned factor;
+    cin >> numBC; // Número de casos
+    
+    cin.get(); //por el retorno de carrol
+    for(int i=0; i<numBC; i++)
+    {
+        std::string linea;
+        getline(cin, linea);
+        cout <<  strtok(char*(linea), ":") << endl;  
+        cout <<  strtok(char*(linea), "Entonces") << endl;  
+        cout <<  strtok(char*(linea), ",") << endl;  
+
+        strtok(NULL, "=");
+        cout <<  stof(strtok(NULL, "=")) << endl;    
+
+        if(factor < -1 || factor > 1){
+            cerr << "ERROR" << factor << endl;
+            exit(0);
+        }
+        regla.facCerBC = factor;
+        bc.insert(regla);
+        
+    } 
+}
+
+*/
+
+int 
 main( int argc, char * argv[] )
 {
 
-    
-
+    entradaBH();
+    return 0;
 }
+
+/* Encadenamiento hacia atrás o Dirigido por Metas
+    Determina si se verifica una cierta meta con los hechos disponibles.
+    La inferencia progresa en la red de derecha a izquierda
+    bool 
+    motchar * orInferencias(char *meta, char **BH, char **BC)
+    {
+        //BH = entradaBH;
+        //return Verificar(meta, BH, BC);
+    }
+
+    bool
+    contenida(char *meta, char **BH)
+    {
+        return true;
+    }bool 
+    motorInferencias(char *meta, char **BH, char **BC)
+
+    char **
+    consecuentes(char **BC)
+    {
+
+    }
+
+    char ** 
+    equiparar(char **consecuentes, char *meta)
+    {
+
+    }
+
+    char **
+    consecuentes(char **BC)
+    {
+
+    }
+
+    bool
+    noVacio(char **conjuntoVacio)
+    {
+
+    }
+
+    char *
+    resolver(char **conjuntoConflicto)
+    {
+
+    }
+
+    void
+    eliminar(char *r, char **conjuntoConflicto)
+    {
+
+    }
+
+    char **
+    extraerAntecedentes(char **r)
+    {
+
+    }
+
+    char *
+    seleccionarMeta(char **nuevasMetas)
+    {
+
+
+    }
+
+    void
+    añadir(char *meta, char **BH)
+    {
+
+    }
+
+    bool 
+    verificar(char *meta, char **BH, char **BC)
+    {
+        
+        bool verificado = false;
+        char **conjuntoConflicto = NULL;
+        char **conjuntoVacio = NULL;
+        char *r = NULL;
+        char **nuevasMetas = NULL;
+        char * nMet = NULL;
+
+        if(contenida(meta, BH))
+        {
+            return true;
+
+        }
+        conjuntoConflicto= equiparar(consecuentes(BC), meta);
+        while(noVacio(conjuntoVacio))
+        {
+            r=resolver(conjuntoConflicto);
+            eliminar(r,conjuntoConflicto);
+            nuevasMetas= extraerAntecedentes(r);
+            verificado=true;
+            while(noVacio(nuevasMetas) && verificado)
+            {
+                nMet=seleccionarMeta(nuevasMetas);
+                eliminar(nMet, nuevasMetas);
+                verificado=verificar(nMet,BH);
+            }
+            if (verificado)
+            {
+                añadir(meta, BH);
+            }
+        }
+        return(verificado);
+    }
+*/
 
 
