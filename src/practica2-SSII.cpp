03f1704e989743f22a7652157bde7efe3ff5dadf
Elena Pérez González-Tablas <elenatablas1@gmail.com>
2021-12-10 00:43:21 +0000

    src: segmentation fault en entrada

--- src/practica2-SSII.cpp
+++ src/practica2-SSII.cpp
@@ -4,6 +4,7 @@
 #include <regex>
 #include <string>
 #include <set>
+#include <functional>
 
 using namespace std;
 
@@ -15,37 +16,44 @@ using namespace std;
 #define NOM_HECHO 1
 #define FACTOR_CERTEZA_HECHO 3
 
-enum 
-TipoRep { literal, conjuncion, disyuncion };
-
-struct 
-ReglaRep
+enum TipoRep
 {
-    int numRegla;
-    std::set<std::string> antecedentes;
-    int numAntecedentes;
-    enum TipoRep tipo;
-    std::string consecuente;
-    float facCerBC;
+    literal,
+    conjuncion,
+    disyuncion
 };
-
-struct 
-HechoRep
-{
-    std::string nomHecho;
-    float facCerBH;
-};
-
 typedef enum TipoRep Tipo;
-typedef struct ReglaRep Regla;
-typedef struct HechoRep Hecho;
+
+class
+    Regla
+{
+    public:
+        int numRegla;
+        std::set<std::string> antecedentes;
+        int numAntecedentes;
+        enum TipoRep tipo;
+        std::string consecuente;
+        float facCerBC;
+    bool operator < (const Regla& rhs) const {return numRegla<rhs.numRegla;}
+};
+
+class
+    Hecho
+{
+    public:
+        std::string nomHecho;
+        float facCerBH;
+    bool operator < (const Hecho& rhs) const {return facCerBH<rhs.facCerBH;}
+};
+
+
 
 std::string entradaBH(std::set<Hecho> bh, std::ifstream &ficheroBH);
 void entradaBC(std::set<Regla> bc, std::ifstream &ficheroBC);
-bool verificar(std::string objetivo, std::set<Regla> bc, std::set<Hecho> bh);
+bool motorInferencias(std::string objetivo, std::set<Regla> bc, std::set<Hecho> bh);
 
 int 
-main( int argc, char * argv[] )
+main(int argc, char *argv[])
 {
     std::set<Regla> bc;
     std::set<Hecho> bh;
@@ -56,25 +64,25 @@ main( int argc, char * argv[] )
     ficheroBH.open(fichBH, ifstream::in);
     if (!ficheroBC.is_open() || !ficheroBH.is_open())
     {
-        cerr << "ERROR: fichero de entrada inexistente" <<  endl;
+        cerr << "ERROR: fichero de entrada inexistente" << endl;
         exit(0);
     }
     std::ofstream ficheroSalida;
-    ficheroSalida.open(fichBC.append(fichBH), ofstream::out);
+    ficheroSalida.open(fichBC+fichBH, ofstream::out);
     ficheroSalida << "Fichero BC: " << fichBC << endl;
     ficheroSalida << "Fichero BH: " << fichBH << endl;
 
-    entradaBC(bc, ficheroBC);
+    //entradaBC(bc, ficheroBC);
     std::string objetivo = entradaBH(bh, ficheroBH);
 
     ficheroSalida << "Objetivo: " << objetivo << endl;
     ficheroSalida << "Proceso de Inferencia: " << endl;
-    ficheroSalida << "Como se activa la red: " << endl;
+    ficheroSalida << "Cómo se activa la red: " << endl;
     ficheroSalida << "CASO-i de inferencia que se va aplicando: " << endl;
     ficheroSalida << "Hecho Objetivo con su FC: " << endl;
 
-    //verificar(objetivo, bc, bh);
-    
+    //motorInferencias(objetivo, bc, bh);
+
     ficheroBC.close();
     ficheroBH.close();
     ficheroSalida.close();
@@ -93,41 +101,46 @@ entradaBC(std::set<Regla> bc, std::ifstream &ficheroBC)
     unsigned numBC;
     std::string linea;
     ficheroBC >> numBC; // Número de casos
-    ficheroBC.get(); //por el retorno de carrol
-    for(int i=0; i<numBC && ! ficheroBC.eof(); i++)
+    ficheroBC.get();    // por el retorno de carrol
+    for (int i = 0; i < numBC && !ficheroBC.eof(); i++)
     {
         getline(ficheroBC, linea);
         std::sregex_token_iterator num_regla(linea.begin(), linea.end(), regla_regex, NUM_REGLA);
         std::sregex_token_iterator antecedentes_regla(linea.begin(), linea.end(), regla_regex, ANTECEDENTES_REGLA);
         std::sregex_token_iterator consecuente_regla(linea.begin(), linea.end(), regla_regex, CONSECUENTE_REGLA);
         std::sregex_token_iterator factor_certeza_regla(linea.begin(), linea.end(), regla_regex, FACTOR_CERTEZA_REGLA);
-        
+
         regla.numRegla = stoi(num_regla->str());
-        
+
         std::string antecedentes = antecedentes_regla->str();
 
         std::smatch literal;
-        if(!std::regex_search(antecedentes, literal, regla_no_literal)){
+        if (!std::regex_search(antecedentes, literal, regla_no_literal))
+        {
             regla.antecedentes.insert(antecedentes);
             regla.tipo = Tipo::literal;
-            regla.numAntecedentes=1;
+            regla.numAntecedentes = 1;
             std::cout << "regla.literal " << antecedentes << "\n";
         }
         else
         {
             std::smatch conjuncion;
             int numAntecedentes = 0;
-            if(std::regex_search(antecedentes, conjuncion, regla_conjuncion)){
-                regla.tipo = Tipo::conjuncion;  
+            if (std::regex_search(antecedentes, conjuncion, regla_conjuncion))
+            {
+                regla.tipo = Tipo::conjuncion;
             }
-            else{
+            else
+            {
                 regla.tipo = Tipo::disyuncion;
             }
             auto dis_begin = std::sregex_iterator(antecedentes.begin(), antecedentes.end(), regex_ant);
             auto dis_end = std::sregex_iterator();
-            for (std::sregex_iterator i = dis_begin; i != dis_end; ++i) {
-                std::smatch match = *i; 
-                if(match.str() != "y" && match.str() != "o") {                                               
+            for (std::sregex_iterator i = dis_begin; i != dis_end; ++i)
+            {
+                std::smatch match = *i;
+                if (match.str() != "y" && match.str() != "o")
+                {
                     regla.antecedentes.insert(match.str());
                     std::cout << "regla.conj/dis " << match.str() << "\n";
                     numAntecedentes++;
@@ -138,7 +151,8 @@ entradaBC(std::set<Regla> bc, std::ifstream &ficheroBC)
 
         regla.consecuente = consecuente_regla->str();
         regla.facCerBC = stof(factor_certeza_regla->str());
-        if(regla.facCerBC < -1 || regla.facCerBC > 1){
+        if (regla.facCerBC < -1 || regla.facCerBC > 1)
+        {
             cerr << "ERROR: factor de certeza incorrecto" << regla.facCerBC << endl;
             exit(0);
         }
@@ -156,136 +170,129 @@ entradaBH(std::set<Hecho> bh, std::ifstream &ficheroBH)
     unsigned numBH;
     std::string linea;
     ficheroBH >> numBH; // Número de casos
-    ficheroBH.get(); //por el retorno de carrol
-    for(int i=0; i<numBH && ! ficheroBH.eof(); i++)
+    ficheroBH.get();    // por el retorno de carrol
+    for (int i = 0; i < numBH && !ficheroBH.eof(); i++)
     {
         getline(ficheroBH, linea);
         std::sregex_token_iterator nombre_hecho(linea.begin(), linea.end(), hecho_linea, NOM_HECHO);
         std::sregex_token_iterator factor_certeza_hecho(linea.begin(), linea.end(), hecho_linea, FACTOR_CERTEZA_HECHO);
         hecho.nomHecho = nombre_hecho->str();
         hecho.facCerBH = stof(factor_certeza_hecho->str());
-        if(hecho.facCerBH < -1 || hecho.facCerBH > 1){
+        if (hecho.facCerBH < -1 || hecho.facCerBH > 1)
+        {
             cerr << "ERROR: factor de certeza incorrecto" << hecho.facCerBH << endl;
             exit(0);
         }
         std::cout << "regla.numRegla " << hecho.facCerBH << "\n";
         bh.insert(hecho);
-
     }
     std::string objetivo;
     getline(ficheroBH, objetivo);
     getline(ficheroBH, objetivo);
     return objetivo;
 }
+/*
+//Encadenamiento hacia atrás o Dirigido por Metas
+//   Determina si se verifica una cierta meta con los hechos disponibles.
+//    La inferencia progresa en la red de derecha a izquierda 
+bool verificar(std::string objetivo, std::set<Regla> bc, std::set<Hecho> bh);
 
-/* Encadenamiento hacia atrás o Dirigido por Metas
-    Determina si se verifica una cierta meta con los hechos disponibles.
-    La inferencia progresa en la red de derecha a izquierda
-    bool 
-    motchar * orInferencias(char *meta, char **BH, char **BC)
-    {
-        //BH = entradaBH;
-        //return Verificar(meta, BH, BC);
-    }
+bool 
+motorInferencias(std::string objetivo, std::set<Regla> bc, std::set<Hecho> bh)
+{
+    bool encontrado = verificar(objetivo, bc, bh);
+    return encontrado;
+}
 
-    bool
-    contenida(char *meta, char **BH)
+bool 
+contenida(std::string objetivo, std::set<Hecho> bh)
+{
+    return true;
+}
+
+std::string
+consecuentes(std::set<Regla> bc)
+{
+    return NULL;
+}
+
+std::string
+equiparar(std::set<Regla> bc, std::string objetivo)
+{
+    return NULL;
+}
+
+std::string
+consecuentes(std::set<Regla> bc)
+{
+    return NULL;
+}
+
+bool 
+noVacio(std::set<Regla> conjuntoVacio)
+{
+    return true;
+}
+
+std::string
+resolver(std::set<Regla> conjuntoConflicto)
+{
+    return NULL;
+}
+
+void 
+eliminar(std::string r, std::set<Regla> conjuntoConflicto)
+{
+}
+
+std::string
+extraerAntecedentes(std::set<std::string> r)
+{
+}
+
+std::string
+seleccionarMeta(std::set<Hecho> nuevasMetas)
+{
+}
+
+void 
+añadir(std::string objetivo, std::set<Hecho> bh)
+{
+}
+
+bool 
+verificar(std::string objetivo, std::set<Regla> bc, std::set<Hecho> bh ))
+{
+
+    bool verificado = false;
+    std::set<Regla> conjuntoConflicto = NULL;
+    std::set<Regla> conjuntoVacio = NULL;
+    std::stringr = NULL;
+    std::set<Hecho>nuevasMetas = NULL;
+    std::string nMet = NULL;
+
+    if (contenida(objetivo, bh))
     {
         return true;
-    }bool 
-    motorInferencias(char *meta, char **BH, char **BC)
-
-    char **
-    consecuentes(char **BC)
-    {
-
     }
-
-    char ** 
-    equiparar(char **consecuentes, char *meta)
+    conjuntoConflicto = equiparar(consecuentes(bc), objetivo);
+    while (noVacio(conjuntoVacio))
     {
-
-    }
-
-    char **
-    consecuentes(char **BC)
-    {
-
-    }
-
-    bool
-    noVacio(char **conjuntoVacio)
-    {
-
-    }
-
-    char *
-    resolver(char **conjuntoConflicto)
-    {
-
-    }
-
-    void
-    eliminar(char *r, char **conjuntoConflicto)
-    {
-
-    }
-
-    char **
-    extraerAntecedentes(char **r)
-    {
-
-    }
-
-    char *
-    seleccionarMeta(char **nuevasMetas)
-    {
-
-
-    }
-
-    void
-    añadir(char *meta, char **BH)
-    {
-
-    }
-
-    bool 
-    verificar(std::string objetivo, std::set<Regla> bc, std::set<Hecho> bh)
-    {
-        
-        bool verificado = false;
-        char **conjuntoConflicto = NULL;
-        char **conjuntoVacio = NULL;
-        char *r = NULL;
-        char **nuevasMetas = NULL;
-        char * nMet = NULL;
-
-        if(contenida(meta, BH))
+        r = resolver(conjuntoConflicto);
+        eliminar(r, conjuntoConflicto);
+        nuevasMetas = extraerAntecedentes(r);
+        verificado = true;
+        while (noVacio(nuevasMetas) && verificado)
         {
-            return true;
-
+            nMet = seleccionarMeta(nuevasMetas);
+            eliminar(nMet, nuevasMetas);
+            verificado = verificar(nMet, bh);
         }
-        conjuntoConflicto= equiparar(consecuentes(BC), meta);
-        while(noVacio(conjuntoVacio))
+        if (verificado)
         {
-            r=resolver(conjuntoConflicto);
-            eliminar(r,conjuntoConflicto);
-            nuevasMetas= extraerAntecedentes(r);
-            verificado=true;
-            while(noVacio(nuevasMetas) && verificado)
-            {
-                nMet=seleccionarMeta(nuevasMetas);
-                eliminar(nMet, nuevasMetas);
-                verificado=verificar(nMet,BH);
-            }
-            if (verificado)
-            {
-                añadir(meta, BH);
-            }
+            añadir(objetivo, bh);
         }
-        return(verificado);
     }
+    return (verificado);
+}
 */
-
-
